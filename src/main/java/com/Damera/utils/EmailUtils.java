d9448cd29866e4743035206ff7d3ee6b78600b15
saathvikdamera <saathvikdamera@gmail.com>
2023-04-29 11:59:46 +0530

    Final commit

--- src/main/java/com/Damera/utils/EmailUtils.java
+++ src/main/java/com/Damera/utils/EmailUtils.java
@@ -6,39 +6,38 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class EmailUtils {
-	
+
 	@Autowired
 	private JavaMailSender emailSender;
-	
+
 	public void sendEmailWithAttachment(String to,
 			String subject, 
 			String text,
 			File file) throws MessagingException {
-	    MimeMessage message = emailSender.createMimeMessage();
+		MimeMessage message = emailSender.createMimeMessage();
 
-	    // use the true flag to indicate you need a multipart message
-	    MimeMessageHelper helper = new MimeMessageHelper(message, true);
+		// use the true flag to indicate you need a multipart message
+		MimeMessageHelper helper = new MimeMessageHelper(message, true);
 
-	    helper.setFrom("saathvikdamera@gmail.com");
-	    helper.setTo(to);
-	    helper.setSubject(subject);
-	    helper.setText(text);
+		helper.setFrom("saathvikdamera@gmail.com");
+		helper.setTo(to);
+		helper.setSubject(subject);
+		helper.setText(text);
 
-	    // attach the file
-	   // FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
-	    helper.addAttachment("Plans", file);
+		// attach the file
+		// FileSystemResource file = new FileSystemResource(new File(pathToAttachment));
+		helper.addAttachment("Plans", file);
 
-	    emailSender.send(message);
-	    System.out.println("Mail with attachment sent successfully");
+		emailSender.send(message);
+		System.out.println("Mail with attachment sent successfully");
 
 
-}
+	}
 
 }
